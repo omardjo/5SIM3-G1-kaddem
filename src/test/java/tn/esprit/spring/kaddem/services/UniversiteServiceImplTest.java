0fb4ef504705c3edbae5882074411ffdc6f99f00
yassine <yassineezzar4@gmail.com>
2024-11-01 16:47:02 +0000

    commiitt

--- src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
@@ -1,29 +1,26 @@
 package tn.esprit.spring.kaddem.services;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.entities.Universite;
-import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 import tn.esprit.spring.kaddem.repositories.UniversiteRepository;
-import tn.esprit.spring.kaddem.services.UniversiteServiceImpl;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import java.util.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class UniversiteServiceImplTest {
 
     @Mock
-   private UniversiteRepository universiteRepository;
-
-    @Mock
-    private DepartementRepository departementRepository;
+    private UniversiteRepository universiteRepository;
 
     @InjectMocks
     private UniversiteServiceImpl universiteService;
@@ -79,49 +76,6 @@ public class UniversiteServiceImplTest {
         universiteService.deleteUniversite(1);
 
         verify(universiteRepository, never()).delete(any(Universite.class));
-    }
-
-    @Test
-    public void testAssignUniversiteToDepartement() {
-        Departement departement = new Departement(1, "Informatique");
-        Set<Departement> departements = new HashSet<>();
-        departements.add(departement);
-        universite.setDepartements(departements);
-
-        when(universiteRepository.findById(1)).thenReturn(Optional.of(universite));
-        when(departementRepository.findById(1)).thenReturn(Optional.of(departement));
-
-        universiteService.assignUniversiteToDepartement(1, 1);
-
-        verify(universiteRepository, times(1)).save(universite);
-        assertTrue(universite.getDepartements().contains(departement));
-    }
-
-    @Test
-   public void testRetrieveDepartementsByUniversite() {
-        Departement departement = new Departement(1, "Informatique");
-        Set<Departement> departements = new HashSet<>();
-        departements.add(departement);
-        universite.setDepartements(departements);
-
-        when(universiteRepository.findById(1)).thenReturn(Optional.of(universite));
-
-        Set<Departement> result = universiteService.retrieveDepartementsByUniversite(1);
-
-        assertNotNull(result);
-        assertEquals(1, result.size());
-        assertTrue(result.contains(departement));
-        verify(universiteRepository, times(1)).findById(1);
-    }
-
-    @Test
-    public void testRetrieveDepartementsByUniversiteNotFound() {
-        when(universiteRepository.findById(1)).thenReturn(Optional.empty());
-
-        Set<Departement> result = universiteService.retrieveDepartementsByUniversite(1);
-
-        assertNull(result);
-        verify(universiteRepository, times(1)).findById(1);
     }
 
     @Test
