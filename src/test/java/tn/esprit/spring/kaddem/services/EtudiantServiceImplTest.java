3950a314635f0e3c1d448878d693b82205f0a2b3
omardjo <djebbi.omar@esprit.tn>
2024-11-09 22:24:35 +0100

    fixed tests

--- src/test/java/tn/esprit/spring/kaddem/services/EtudiantServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/EtudiantServiceImplTest.java
@@ -17,7 +17,7 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class EtudiantServiceImplTest {
